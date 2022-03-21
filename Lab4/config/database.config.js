5aac85ed037a8edbe82778c841f526458fda4523
Muhammad Fayaad <fayet256@gmail.com>
2022-03-21 21:56:39 +0700

    db update

--- Lab4/config/database.config.js
+++ Lab4/config/database.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  url: 'mongodb://localhost:27017/lab4law'
+  // url: 'mongodb://localhost:27017/lab4law'
+  // url: 'mongodb://localhost:48433/lab4law' // Port Free 3
+  url: 'mongodb+srv://fayaad:fayaad@lab4law.ahydg.mongodb.net/lab4law?retryWrites=true&w=majority'
 }
