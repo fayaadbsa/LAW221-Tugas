8262190519e47a332ec7a2de499bc819fd6c74b8
Muhammad Fayaad <fayet256@gmail.com>
2022-03-23 09:08:52 +0700

    tugas 2

---
+++ Tugas2/StressTest.js
@@ -0,0 +1,43 @@
+import http from "k6/http";
+import { sleep } from "k6";
+
+export let options = {
+  insecureSkipTLSVerify: true,
+  noConnectionReuse: false,
+  stages: [
+    { duration: "1m", target: 100 },
+    { duration: "2m", target: 100 },
+    { duration: "1m", target: 200 },
+    { duration: "2m", target: 200 },
+    { duration: "1m", target: 300 },
+    { duration: "2m", target: 300 },
+    { duration: "1m", target: 400 },
+    { duration: "5m", target: 400 },
+    { duration: "5m", target: 0 },
+  ],
+};
+
+const API_BASE_URL =
+  "http://host-1906398433-port-58433.proxy.infralabs.cs.ui.ac.id";
+
+export default () => {
+  const response = http.post(
+    `${API_BASE_URL}/mahasiswa/`,
+    JSON.stringify({
+      nama: "fayaad",
+      pilihan1: "ddp-a",
+      pilihan2: "matdas-a",
+    }),
+    { headers: { "Content-Type": "application/json" } }
+  );
+
+  const mahasiswaId = JSON.parse(response.body).id
+
+  http.batch([
+    ["GET", `${API_BASE_URL}/mahasiswa/${mahasiswaId}`],
+  ]);
+
+  sleep(1);
+};
+
+// Referensi : https://www.youtube.com/watch?v=r-Jte8Y8zag
